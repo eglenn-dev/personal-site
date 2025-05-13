5ed62513bff08f1788159ece313ca027337ff9eb
Ethan Glenn <ethan+git@eglenn.dev>
2025-05-12 23:13:04 -0600

    Added Vercel speed insights tracking to all pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 import GoogleAnalytics from "@/components/analytics";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -35,6 +36,7 @@ export default function RootLayout({
                 </ThemeProvider>
                 <GoogleAnalytics />
                 <Analytics />
+                <SpeedInsights />
             </body>
         </html>
     );
