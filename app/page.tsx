b236b48706aa88aeff4e7d8fb53a1cb189b2c06c
Ethan Glenn <ethan+git@eglenn.dev>
2026-02-12 21:59:57 -0700

    Layout update

--- app/page.tsx
+++ app/page.tsx
@@ -22,9 +22,9 @@ export default async function Home() {
 
     return (
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
-            <h1 className="text-4xl font-bold mb-6">Ethan Glenn</h1>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mb-12">
                 <div id="main" className="flex flex-col gap-4">
+                    <h1 className="text-4xl font-bold">Ethan Glenn</h1>
                     <h2 className="text-2xl font-semibold">
                         Full-Stack Engineer
                     </h2>
